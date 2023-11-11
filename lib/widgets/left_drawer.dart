c41d79baead10f43f310a02cba4be93ad19f1687
RobertBenyamin <robertbenyamino@gmail.com>
2023-11-11 23:18:05 +0700

    fix: apply safe area to drawer

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -8,70 +8,72 @@ class LeftDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Color(0xFF1f2937),
-            ),
-            child: Column(
-              children: [
-                Text(
-                  'Dekatriamart Warehouse',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF1f2937),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Dekatriamart Warehouse',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Padding(padding: EdgeInsets.all(10)),
-                Text(
-                  "Catat seluruh inventaris anda di sini!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal,
+                  Padding(padding: EdgeInsets.all(10)),
+                  Text(
+                    "Catat seluruh inventaris anda di sini!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.home_outlined),
-            title: const Text('Halaman Utama'),
-            // Bagian redirection ke MyHomePage
-            onTap: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MyHomePage(),
-                  ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.checklist),
-            title: const Text('Lihat Item'),
-            // Bagian redirection ke ShopFormPage
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const ItemListPage();
-              }));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.edit_note),
-            title: const Text('Tambah Item'),
-            // Bagian redirection ke ShopFormPage
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const ItemFormPage();
-              }));
-            },
-          ),
-        ],
+            ListTile(
+              leading: const Icon(Icons.home_outlined),
+              title: const Text('Halaman Utama'),
+              // Bagian redirection ke MyHomePage
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyHomePage(),
+                    ));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.checklist),
+              title: const Text('Lihat Item'),
+              // Bagian redirection ke ShopFormPage
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const ItemListPage();
+                }));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit_note),
+              title: const Text('Tambah Item'),
+              // Bagian redirection ke ShopFormPage
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const ItemFormPage();
+                }));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
